66b19c71b9361b8b0844ae215c1ac5aff2ae0a0d
niyazidageek <niyazinb.code.edu.az>
2021-08-04 11:39:27 +0400

    responsivity of homepage is done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -62,7 +62,7 @@ const HomePage = () => {
                     </div>
                     <div className="row mt-5">
                         <div className="col-md-5">
-                            <img style={{ height: '400px' }} src="https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/book-sq.jpg" alt="" />
+                            <img src="https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/book-sq.jpg" alt="" />
                             <div className="__description">
                                 <h2>Furniture source book</h2>
                                 <span>$39.90</span>
@@ -70,7 +70,7 @@ const HomePage = () => {
                             </div>
                         </div>
                         <div className="col-md-7 d-flex justify-content-end">
-                            <img style={{ height: '550px' }} src="https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/chair-2-sq.jpg" alt="" />
+                            <img src="https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/chair-2-sq.jpg" alt="" />
                             <div className="__description--float">
                                 <h2>Furniture source book</h2>
                                 <span>$39.90</span>
@@ -80,7 +80,7 @@ const HomePage = () => {
                     </div>
                     <div className="row mt-5">
                         <div className="col-md-7 --right d-flex">
-                            <img style={{ height: '550px' }} src="https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/clock-sq.jpg" alt="" />
+                            <img src="https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/clock-sq.jpg" alt="" />
                             <div className="__description--float">
                                 <h2>Sunn Clock</h2>
                                 <span>$89.90</span>
@@ -88,7 +88,7 @@ const HomePage = () => {
                             </div>
                         </div>
                         <div className="col-md-5">
-                            <img style={{ height: '400px' }} src="https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/box-sq.jpg" alt="" />
+                            <img src="https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/box-sq.jpg" alt="" />
                             <div className="__description">
                                 <h2>Furniture source book</h2>
                                 <span>$39.90</span>
@@ -98,7 +98,7 @@ const HomePage = () => {
                     </div>
                     <div className="row mt-5 mb-5">
                         <div className="col-md-5">
-                            <img style={{ height: '400px' }} src="https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/clock2-sq.jpg" alt="" />
+                            <img src="https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/clock2-sq.jpg" alt="" />
                             <div className="__description">
                                 <h2>Furniture source book</h2>
                                 <span>$39.90</span>
@@ -106,7 +106,7 @@ const HomePage = () => {
                             </div>
                         </div>
                         <div className="col-md-7 d-flex justify-content-end">
-                            <img style={{ height: '550px' }} src="https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/chair3-3-sq.jpg" alt="" />
+                            <img src="https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/chair3-3-sq.jpg" alt="" />
                             <div className="__description--float">
                                 <h2>Furniture source book</h2>
                                 <span>$39.90</span>
@@ -117,9 +117,9 @@ const HomePage = () => {
                 </div>
             </div>
             <div className="__deal-of-week">
-                <div className="container d-flex">
-                    <div className="row justify-content-between w-100">
-                        <div className="col-sm-6  d-flex justify-content-between flex-column">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-6 d-flex flex-column">
                             <div className="__title">
                                 <h5>DEAL OF THE WEEK</h5>
                                 <h1>
@@ -153,7 +153,7 @@ const HomePage = () => {
                                 <span>SHOP NOW</span>
                             </div>
                         </div>
-                        <div className="col-6">
+                        <div className="col-sm-6">
                             <img src="https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/chair2-transparent.png" alt="" />
                         </div>
                     </div>
@@ -166,50 +166,50 @@ const HomePage = () => {
                         <h2>New Arrivals</h2>
                         <p>One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections</p>
                         <div className="row">
-                            <div className="col-md-3">
+                            <div className="col-xl-3 col-md-4 col-6">
                                 <img src="https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/chair.jpg" alt="" />
                                 <h5>Norwegg Chair</h5>
                                 <span>$40.00</span>
                             </div>
-                            <div className="col-md-3">
+                            <div className="col-xl-3 col-md-4 col-6">
                                 <img src="https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/book.jpg" alt="" />
                                 <h5>Book</h5>
                                 <span>$40.00</span>
                             </div>
-                            <div className="col-md-3">
+                            <div className="col-xl-3 col-md-4 col-6">
                                 <img src="https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/chair2.jpg" alt="" />
                                 <h5>Another chair</h5>
                                 <span>$40.00</span>
                             </div>
-                            <div className="col-md-3">
+                            <div className="col-xl-3 col-md-4 col-6">
                                 <img src="https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/chair3.jpg" alt="" />
                                 <h5>Your new chair</h5>
                                 <span>$40.00</span>
                             </div>
 
 
-                            <div className="row">
-                                <div className="col-md-3">
+                            {/* <div className="row"> */}
+                                <div className="col-xl-3 col-md-4 col-6">
                                     <img src="https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/clock.jpg" alt="" />
                                     <h5>Clock</h5>
                                     <span>$40.00</span>
                                 </div>
-                                <div className="col-md-3">
+                                <div className="col-xl-3 col-md-4 col-6">
                                     <img src="https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/clock3.jpg" alt="" />
                                     <h5>Clock</h5>
                                     <span>$40.00</span>
                                 </div>
-                                <div className="col-md-3">
+                                <div className="col-xl-3 col-md-4 col-6">
                                     <img src="https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/box.jpg" alt="" />
                                     <h5>Accesory box</h5>
                                     <span>$40.00</span>
                                 </div>
-                                <div className="col-md-3">
+                                <div className="col-xl-3 col-md-4 col-6">
                                     <img src="https://d19m59y37dris4.cloudfront.net/varkala/1-2-1/img/product/box2.jpg" alt="" />
                                     <h5>Biege</h5>
                                     <span>$40.00</span>
                                 </div>
-                            </div>
+                            {/* </div> */}
 
 
                         </div>
