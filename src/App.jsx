544b0906581a14d751d34f8767b367fb5f2a6acc
niyazidageek <niyazinb.code.edu.az>
2021-08-11 18:33:16 +0400

    cdns replaced with npms

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Redirect, Route } from "react-router-dom";
 import "./assets/scss/main.scss"
+import 'aos/dist/aos.css';
 import HomePage from "./pages/HomePage";
 import BlogPage from "./pages/BlogPage";
 import AboutPage from "./pages/AboutPage";
@@ -9,6 +10,9 @@ import { CartProvider } from "react-use-cart";
 import CartPage from "./pages/CartPage";
 import WishlistPage from "./pages/WishlistPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 
 function App() {
   return (
